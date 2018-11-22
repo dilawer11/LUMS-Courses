a2f927352cc0f592cb59226e49ee112032d84d02
Dilawer Ahmed <dilawer11@gmail.com>
2018-11-23 01:08:14 +0500

    added some basic functions

--- OS/PA4/part_1/main.c
+++ OS/PA4/part_1/main.c
@@ -1,12 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MEMSIZE = 16384; //16KB
-#define VIRTUALSIZE = 65536; // 64KB
-#define PAGESIZE = 256; // 256 bytes
-#define PAGETABLESIZE = 512 // 512 bytes 
+#define MEMSIZE 16384 //16KB
+#define VIRTUALSIZE 65536 // 64KB
+#define PAGESIZE 256 // 256 bytes
+#define PAGETABLESIZE 512 // 512 bytes 
+int getPageNumber(int address){		//gets the page entry number in the page table
+	int mask = 256;
+	address>>=8;				
+	return address & mask;
+}
+int getOffset(int address){		//gets the offset from an address 
+	int mask=256;
+	return address & mask;		
+}
+int checkInMemory(char val){	//checks if the page you're asking for is now in memory
+	int mask=1;					
+	return mask & val;
+}	
+int checkDirty(char val){		//checks if dirty bit is on
+	int mask=2;					
+	return mask & val;	
+}
+void setUsed(char * val){		//sets the used bit to 1 in enchanhed page replacement technique
+	int mask=8;
+	*val |= 8;
+}
+int bringPageIntoMemory(int address,char * pageTable,int pageNum){ //brings the page from backing store to memory
+	return 0;
+}
 
-
+int getFrame(int address,char * pageTable){
+	if (address>=VIRTUALSIZE || address<0 ){
+		return 0;
+		printf("Invalid Address Access\n");
+	}
+	else{
+		int pageNum=2*getPageNumber(address);
+		int check,frameAddress;
+		check = pageTable[pageNum];
+		if(checkInMemory(check)){		
+			frameAddress = pageTable[pageNum+1];		//In memory so return the frame address	
+		}
+		else{
+			frameAddress=bringPageIntoMemory(address,pageTable,pageNum); //bring the page table into memory and update the page table accordingly
+ 		}
+ 		setUsed(&pageTable[pageNum]); 
+ 		return frameAddress;
+	}
+}
+int readFromMemory(int frame,int address){		//prototypes can be changed
+	//read function
+	return 0;
+}
+int writeToMemory(int frame,int address,int valToWrite){ //prototypes can be changed
+	//write function
+	return 0;
+}
 int main() {
 	printf("This Works!\n");
 	
@@ -22,7 +72,16 @@ int main() {
 	add[1]=65535;
 	add[2]=65536;
 	add[3]= 20;
-	
+	int frame = getFrame(add[0],mainMemory);
+	if (frame ==0 || 1){
+		printf("Error frame is supposed to be used to store page table\n");
+	}
+	else{
+		//read or write at that frame whatever you want to do;
+	}
+
+
+
 	free(mainMemory);
 	return 0;
 }
