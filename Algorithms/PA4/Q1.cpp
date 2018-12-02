d4e0cf41f21071786a3972e155b89108b79888dd
Dilawer Ahmed <dilawer11@gmail.com>
2018-12-02 05:40:04 +0500

    PA4 Complete Testing left

--- Algorithms/PA4/Q1.cpp
+++ Algorithms/PA4/Q1.cpp
@@ -6,16 +6,25 @@ using namespace std;
 
 ifstream fin;
 ofstream fout;
+
+struct retVal{
+	string prevVals;
+	int maxVal;
+	retVal(){
+		prevVals="";
+		maxVal=-1;
+	}
+};
 class Q1{
 public:
 	Q1(string fileName);
 	~Q1();
 private:
-	int maxProduction(int i);
+	retVal maxProduction(int i);
 	int n;
 	int* high;
 	int* low;
-	string* memo;
+	retVal* memo;
 
 };
 Q1::Q1(string fileName){
@@ -24,11 +33,10 @@ Q1::Q1(string fileName){
 	fin >> temp >> n;
 	high = new int[n];
 	low = new int[n];
-	memo = new string[n];
+	memo = new retVal[n];
 	for(int i=0;i<n;i++){
 		high[i]=0;
 		low[i]=0;
-		memo[i]="";
 	}
 	fin >> temp;
 	for(int i=0;i<n;i++){
@@ -39,51 +47,53 @@ Q1::Q1(string fileName){
 		fin >> low[i];
 	}
 	fin.close();
-	int val = maxProduction(n);
-	cout << "Total Revenue: " << val << endl;
+	retVal val = maxProduction(n);
+	cout << val.prevVals;
+	cout << "Total Revenue: " << val.maxVal << endl;
 }
 Q1::~Q1(){
 	if(high)delete [] high;
 	if(low)delete [] low;
 	if(memo)delete [] memo;
 }
-int Q1::maxProduction(int i){
+retVal Q1::maxProduction(int i){
 	if(i==0){
-		return 0;
+		retVal ret;
+		ret.prevVals="";
+		ret.maxVal=0;
+		return ret;
 	}
-	else if (memo[i-1]!=""){
-		int spacePos = memo[i-1].find(" ");
-		int retVal = stoi(memo[i-1].substr(spacePos+1,memo[i-1].length()-1-spacePos));
-
-		return retVal;
+	else if (memo[i-1].maxVal!=-1){
+		return memo[i-1];
 	}
 	else if(i==1){
+		retVal ret;
 		if(high[0]>=low[0]){
-			memo[0]="High "+to_string(high[0]);
-			cout << "Week 1: High " << high[0] << endl;
-
-			return high[0];
+			ret.prevVals = "Week 1: High " + to_string(high[0])+"\n";
+			ret.maxVal=high[0];
 		}
 		else{
-			memo[0]="Low "+to_string(low[0]);
-			cout << "Week 1: Low " << low[0] << endl;
-			return low[0];
+			ret.prevVals = "Week 1: Low " + to_string(low[0])+"\n";
+			ret.maxVal=low[0];
 		}
+		memo[i-1]=ret;
+		return ret;
 	}
 	else{
-		int rh = high[i-1]+maxProduction(i-2);
-		int rl = low[i-1]+maxProduction(i-1);
-		int max = rl;
-		if(rh>rl){
-			memo[i-1] = "High "+to_string(rh);
-			max=rh;
+		retVal rh = maxProduction(i-2);
+		retVal rl = maxProduction(i-1);
+		if(rh.maxVal+high[i-1]>rl.maxVal+low[i-1]){
+			rh.prevVals += "Week "+to_string(i-1)+": Priming\nWeek " +to_string(i)+": High "+to_string(high[i-1])+"\n";
+			rh.maxVal+=high[i-1];
+			memo[i-1]=rh;
+			return rh;
 		}
 		else{
-			memo[i-1] = "Low "+to_string(rl);
+			rl.prevVals += "Week " +to_string(i)+": Low "+to_string(low[i-1])+"\n";
+			rl.maxVal+=low[i-1];
+			memo[i-1]=rl;
+			return rl;
 		}
-	
-		return max;
-
 	}
 }
 int main(){
